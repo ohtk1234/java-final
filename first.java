2ea0845517c5208daaa12e7b47a86ee598b89c65
ohtk1234 <ohtk1234@naver.com>
2023-02-17 23:55:22 +0900

    first

---
+++ first.java
@@ -0,0 +1,33 @@
+
+public class first {
+
+	public static void main(String[] args) {
+		//변수: 하나의 값을 저장 할 수 있는 메모리 번지에 붙여진 이름
+		//타입 = 이름
+		//int = abc
+		//자바 소스 파일명은 대문자로 시작하는 것이 관례
+		//ex: Weekjava
+		//변수를 초기화 하는 방법
+		//1) 변수를 선언하고 값을 주는 경우
+//		int a;
+//		a = 10;
+//		System.out.println(a);
+		//2)변수를 선언하는 동시에 값을 주는 경우
+//		int x = 5;
+//		System.out.println(x);
+		//자바의 타입
+		//정수:byte, short, char, int, long
+		//실수:float, double
+		//논리 타입: boolean- 참과 거짓으로 결과가 나오는 타입
+		//문자열 타입: String
+		//****자동 타입 변환****
+		//작은 타입이 큰 타입으로 대입될때 발생한다. 자동으로 타입변환이 된다
+		//****강제 타입 변환****
+		//자동과 반대로 큰타입이 작은타입으로 대입될때 발생한다. ()을 이용하여 코딩해주어야한다.
+		
+		
+	}
+
+}//end class
+
+
---
+++ third.java
@@ -0,0 +1,49 @@
+
+public class third {
+
+	public static void main(String[] args) {
+		//조건문
+		//if(조건문){실행문;}
+		//결과가 두번일때만
+		//if(조건문) {
+//		}else {
+//			
+//		}
+		//결과가 여러개일때
+		//if(조건문) {
+//		}else if{
+//			
+//		}else {
+//			
+//		}
+		//if문 중첩
+		//if문 2개이상이 중첩되어 사용됨
+		//랜덤함수 math
+		//0 <=(int)(Math.random() *6) < 6
+		//switch
+		//case를 사용하여 실행문 실행 switch문을 탈출하기 위해서는 break;를 꼭 써줘야한다.
+		//반복문
+		//for문(정해진 횟수만큼 반복)
+		//for(초기화식; 조건식; 증감식){
+				//실행문;
+		//}
+		//while문(조건식이 true일 경우 반복, false이면 반복문 멈춤)
+		//while(조건식){
+				//	실행문;
+		//}
+		
+		//do-while문
+		//while문이 false여도 한번은 꼭 실행
+		//do{
+		//	실행문;
+		//}while(조건식);
+		//break문
+		//for문, while문, do-while문을 실행 중지할때 사용한다.
+		//continue문
+		//continue문이 실행되면 for문의 증감식 또는 while문, do-while문의 조건식으로 바로 이동할때 사용한다.
+		
+		
+
+	}
+
+}
